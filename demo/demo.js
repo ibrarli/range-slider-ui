ae8f0acaf7941b52ea3353f19ca38a8fe1d005f1
Ibrar-Haidar <106898965+Ibrar-Haidar@users.noreply.github.com>
2025-06-06 15:17:15 +0500

    fixed browserify

--- demo/demo.js
+++ demo/demo.js
@@ -1,17 +1,17 @@
-const rangeSlider = require('..')
+const rangeSlider = require("..");
 
-const range = rangeSlider({ min: 0, max: 10 })
-document.body.innerHTML = `<h1> Range Slider </h1>`
+const range = rangeSlider({ min: 0, max: 10 });
+document.body.innerHTML = `<h1> Range Slider </h1>`;
 
-const main = document.createElement('div')
-main.classList.add('demo')
+const main = document.createElement("div");
+main.classList.add("demo");
 
-const style = document.createElement('style')
+const style = document.createElement("style");
 style.textContent = `
   .demo {
     padding: 50px;
   }
-` 
+`;
 
-main.append(range)
-document.body.append(main, style)
+main.append(range);
+document.body.append(main, style);
